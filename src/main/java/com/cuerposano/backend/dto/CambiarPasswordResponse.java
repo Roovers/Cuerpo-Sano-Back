bf61f81377dd8013564dec598ab243d9579b2771
Roovers <91910203+Roovers@users.noreply.github.com>
2026-06-14 22:51:09 -0300

    Subir src/main/java/com/cuerposano/backend/dto/CambiarPasswordResponse.java

---
+++ src/main/java/com/cuerposano/backend/dto/CambiarPasswordResponse.java
@@ -0,0 +1,13 @@
+package com.cuerposano.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CambiarPasswordResponse {
+
+    private String mensaje;
+}
