95e733832021807ce6f5456a00756d734346dabb
Roovers <91910203+Roovers@users.noreply.github.com>
2026-06-14 22:52:40 -0300

    Subir src/main/java/com/cuerposano/backend/repositories/UsuarioRepository.java

---
+++ src/main/java/com/cuerposano/backend/repositories/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.cuerposano.backend.repositories;
+
+import com.cuerposano.backend.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+
+    boolean existsByNombreUsuario(String nombreUsuario);
+
+    long countByRolAndActivoTrue(String rol);
+}
