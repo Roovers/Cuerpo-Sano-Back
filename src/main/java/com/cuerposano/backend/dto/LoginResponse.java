c64d90453779cb55d5e0fbb9eb2fd58040adaddb
Roovers <91910203+Roovers@users.noreply.github.com>
2026-06-14 22:51:30 -0300

    Subir src/main/java/com/cuerposano/backend/dto/LoginResponse.java

---
+++ src/main/java/com/cuerposano/backend/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.cuerposano.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+    private String usuario;
+    private String rol;
+}
